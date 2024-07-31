3f8d5c8f6f5b9c0476dd6368db3284b5b7e8244e
OMAR-AHMED-SAAD <110069095+OMAR-AHMED-SAAD@users.noreply.github.com>
2024-08-01 01:24:13 +0300

    Updated db schema and added a mongo atlas db

--- Server/config.py
+++ Server/config.py
@@ -1,2 +1,3 @@
 class Config:
-    MONGO_URI = "mongodb://localhost:27017/ai_portfolio"
+    # MONGO_URI = "mongodb://localhost:27017/ai_portfolio"
+    MONGO_URI = "mongodb+srv://omarahmed:IWoIAqnokTDJZct3@virtualexhibition.ei77hyx.mongodb.net/"
