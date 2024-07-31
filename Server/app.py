83d69ee499fc1a879e9a078a6a4376c673f23008
OMAR-AHMED-SAAD <110069095+OMAR-AHMED-SAAD@users.noreply.github.com>
2024-08-01 01:25:39 +0300

    Added apis to get all models and model by model path

--- Server/app.py
+++ Server/app.py
@@ -3,18 +3,26 @@ from flask_cors import CORS
 from pymongo import MongoClient
 from controller import *
 from config import Config
+import certifi
 
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
-client = MongoClient(app.config["MONGO_URI"])
-db = client.ai_portfolio
+client = MongoClient(app.config["MONGO_URI"], tlsCAFile=certifi.where())
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+db = client.virtual_exhibition
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
 
 @app.route("/predict", methods=["POST"])
 def predict_fn():
@@ -28,6 +36,7 @@ def predict_fn():
     result = predict(model_name, text)
     return jsonify({"modelName": model_name, "text": text, "result": result})
 
+
 @app.route("/explain", methods=["POST"])
 def explain_fn():
     data = request.get_json()
@@ -40,6 +49,7 @@ def explain_fn():
     result = explain(model_name, text)
     return jsonify({"modelName": model_name, "text": text, "result": result})
 
+
 @app.route("/generate", methods=["POST"])
 def generate_fn():
     data = request.get_json()
@@ -48,17 +58,17 @@ def generate_fn():
 
     if not model_name:
         return jsonify({"error": "Model name and text are required"}), 400
-    
+
     if not text:
         text = None
 
-    result = generate(model_name,text)
+    result = generate(model_name, text)
     return jsonify({"text": text, "image": result})
+
 
 @app.route("/students", methods=["POST"])
 def add_student_fn():
     student_data = request.get_json()
-    print(request)
     if not student_data:
         return jsonify({"error": "Student data is required"}), 400
 
@@ -68,10 +78,12 @@ def add_student_fn():
 
     return jsonify({"_id": result["_id"], "message": "Student added successfully"}), 201
 
+
 @app.route("/students", methods=["GET"])
 def list_students_fn():
     students = list_students(db)
     return jsonify(students), 200
+
 
 @app.route("/students/<student_id>", methods=["GET"])
 def get_student_fn(student_id):
@@ -79,6 +91,24 @@ def get_student_fn(student_id):
     if not student:
         return jsonify({"error": "Student not found"}), 404
     return jsonify(student), 200
+
+
+@app.route("/get_models", methods=["GET"])
+def get_models_fn():
+    students = list_students(db)
+    models = []
+    for student in students:
+        models.append(student["model"])
+    return jsonify(models), 200
+
+
+@app.route("/get_model/<model_path>", methods=["GET"])
+def get_model_fn(model_path):
+    students = list_students(db)
+    for student in students:
+        if student["model"]["path"] == model_path:
+            return jsonify({"model": student.pop('model'), 'student': student}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
