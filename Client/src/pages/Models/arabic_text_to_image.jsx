3d2a24eb6da12e96e06cb6d8237c36081c58a32d
eyadnaserr <eyadnaserrr@gmail.com>
2024-08-05 19:31:06 +0300

    arabic text to image interface

--- Client/src/pages/Models/arabic_text_to_image.jsx
+++ Client/src/pages/Models/arabic_text_to_image.jsx
@@ -1,7 +1,53 @@
-const ArabicTextToImage = () => {
-    return <div>
-        <h1>arabic_text_to_image</h1>
-    </div>
-    }
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import axiosApi from '../../utils/axiosApi';
+import MyLayout from '../MyLayout';
+import { Button } from 'antd';
+//import './arabic_text_to_image.css';
 
-    export default ArabicTextToImage;
+const ArabicTextToImage = () => {
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [generatedImage, setGeneratedImage] = useState(null);
+
+  const onGenerate = () => {
+    setLoading(true);
+    axiosApi
+      .post('/generate_image', { text })
+      .then((response) => {
+        setGeneratedImage(response.data.image);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error generating image:', error);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <MyLayout modelName="Arabic Text to Image" modelDescription="Generate images from Arabic text">
+      <div className="arabic-text-to-image-container">
+        <div className="left-half">
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter Arabic text here"
+          />
+          <Button
+            type="primary"
+            loading={loading}
+            size="large"
+            onClick={onGenerate}
+          >
+            Generate
+          </Button>
+        </div>
+        <div className="right-half">
+          {generatedImage && <img src={`data:image/jpeg;base64,${generatedImage}`} alt="Generated" />}
+        </div>
+      </div>
+    </MyLayout>
+  );
+};
+
+export default ArabicTextToImage;
